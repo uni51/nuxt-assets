d12cfdf52e5c84eb1b4ddf000e861e1e559cd47e
uni51 <uni51@users.noreply.github.com>
2019-12-27 15:58:09 +0900

    Section07：アセット

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <section class="container">
+    <div>
+      <img src="~/assets/cat.jpg">
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+</style>
